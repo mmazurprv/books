a41ca89a2c36bf548b778398675277e864cb69b6
“Michał <m.mazur.prv@gmail.com>
2024-08-27 00:34:47 +0200

    minor changes on main page (image)

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       {/* Adding the image */}
       <Image
         src="/images/books_graph.png" // Path to your image in the public directory
         alt="Books Image" // Alt text for the image
         width={500} // Desired width of the image
         height={300} // Desired height of the image
-        className="my-8" // Optional: Add any Tailwind CSS classes for styling
       />
-
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
     </main>
   );
 }
